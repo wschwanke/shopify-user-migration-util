03826544019c4d4f345823337cb69b84d6eb30d5
Will Schwanke <wschwanke@gmail.com>
2019-01-11 16:12:22 -0600

    Update tsconfig, package.json, and gitignore

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,13 @@
-// Initial typescript file
+/**
+ * External dependencies
+ */
+import minimist from 'minimist';
+
+/**
+ * Internal dependencies
+ */
+import { logger } from './lib/logger';
+
+const argv = minimist(process.argv.slice(2));
+
+logger.info(argv);
